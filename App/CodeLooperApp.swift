8d66e1ca6f148e4d7c9a9b28aa86da8f6ef439fa
Peter Steinberger <steipete@gmail.com>
2025-05-29 01:16:49 +0200

    Fixes SwiftUI warning

--- App/CodeLooperApp.swift
+++ App/CodeLooperApp.swift
@@ -17,7 +17,7 @@ struct CodeLooperApp: App {
         // Perform one-time setup for the logging system.
         Diagnostics.Logger.bootstrap(destination: .console, minLevel: .debug)
 
-        logger.info("CodeLooperApp initialized. ScenePhase: \(scenePhase)")
+        logger.info("CodeLooperApp initialized")
 
         // Check MCP extension versions on startup
         Task {
